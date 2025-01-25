1e1083094d4be86505cee6c086db7c9d199f3992
fastbodin <aslobodin@proton.me>
2025-01-24 16:09:18 -0800

    changed printing to result in no whitespace at line end in each out file

--- src/read_and_print.cpp
+++ src/read_and_print.cpp
@@ -124,26 +124,26 @@ void save_sol(const Fullerene(&F), const int p, const int num_res_faces,
   // number of resonant faces
   out_files_ptr[0] << num_res_faces << endl;
   // print resonant faces
-  out_files_ptr[1] << p << " ";
-  out_files_ptr[2] << num_res_faces - p << " ";
+  out_files_ptr[1] << p;
+  out_files_ptr[2] << num_res_faces - p;
   for (int i = 0; i < F.dual_n; i++) {
     if (fvars[i].get(GRB_DoubleAttr_X) < 0.99)
       continue;
     if (F.dual[i].size == 5) {
-      out_files_ptr[1] << i << " ";
+      out_files_ptr[1] << " " << i;
     } else {
-      out_files_ptr[2] << i << " ";
+      out_files_ptr[2] << " " << i;
     }
   }
   out_files_ptr[1] << endl;
   out_files_ptr[2] << endl;
   // matching edges
-  out_files_ptr[3] << F.n - 6 * (num_res_faces - p) - 5 * p << " ";
+  out_files_ptr[3] << F.n - 6 * (num_res_faces - p) - 5 * p;
   for (int i = 0; i < F.num_edges; i++) {
     if (evars[i].get(GRB_DoubleAttr_X) < 0.99)
       continue;
-    out_files_ptr[3] << F.edges[i].vertices[0] << " " << F.edges[i].vertices[1]
-                     << " ";
+    out_files_ptr[3] << " " << F.edges[i].vertices[0] << " "
+                     << F.edges[i].vertices[1];
   }
   out_files_ptr[3] << endl;
 }
